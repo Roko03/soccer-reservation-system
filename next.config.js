161cecb611c8b81eb93483a57192fe04d1bad7eb
Roko03 <72657685+Roko03@users.noreply.github.com>
2023-08-25 18:11:00 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const path = require("path");
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+
+module.exports = nextConfig;
