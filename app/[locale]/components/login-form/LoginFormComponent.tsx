9132f5efa23573c752a867edf711062fa438fbf1
Roko <rokoponjarac2@gmail.com>
2023-08-27 19:30:51 +0200

    add buttons

--- app/[locale]/components/login-form/LoginFormComponent.tsx
+++ app/[locale]/components/login-form/LoginFormComponent.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useTranslations } from "next-intl";
 import styles from "./LoginFormComponent.module.scss";
+import ButtonComponent from "../button/ButtonComponent";
 
 const LoginFormComponent = () => {
   const t = useTranslations("Index");
@@ -8,6 +9,9 @@ const LoginFormComponent = () => {
     <form className={styles.login_form}>
       <input type="text" placeholder="Email" />
       <input type="text" placeholder={t("password")} />
+      <ButtonComponent variant={"authentication"}>
+        <p>{t("login")}</p>
+      </ButtonComponent>
     </form>
   );
 };
--- app/[locale]/components/registration-form/RegistrationForm.tsx
+++ app/[locale]/components/registration-form/RegistrationForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useTranslations } from "next-intl";
 import styles from "./RegistrationForm.module.scss";
+import ButtonComponent from "../button/ButtonComponent";
 
 const RegistrationForm = () => {
   const t = useTranslations("Index");
@@ -10,6 +11,9 @@ const RegistrationForm = () => {
       <input type="text" placeholder="Email" />
       <input type="text" placeholder={t("password")} />
       <input type="text" placeholder={t("repeatPassword")} />
+      <ButtonComponent variant={"authentication"}>
+        <p>{t("register")}</p>
+      </ButtonComponent>
     </form>
   );
 };
